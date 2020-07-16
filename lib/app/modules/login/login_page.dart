02d424b16f91c042d4e8ec46bf0e9680de177984
Gilvan Henrique <gilvanh.15@gmail.com>
2020-07-16 11:13:42 -0300

    login page and validation

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'login_controller.dart';
 
@@ -11,7 +12,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends ModularState<LoginPage, LoginController> {
-  //use 'controller' variable to access controller
+
+  final _ctrlLogin = TextEditingController();
+  final _ctrlPassword = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +23,113 @@ class _LoginPageState extends ModularState<LoginPage, LoginController> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: <Widget>[
-          Center(child: Text("Login Page"),)
-        ],
+      body: _body(context),
+    );
+  }
+
+  _body(BuildContext context){
+    return Observer(
+      builder: (BuildContext context) {
+        return Form(
+          key: _formKey,
+          child: Container(
+            padding: EdgeInsets.all(25),
+            child: ListView(
+              children: <Widget>[
+                _textFormField(
+                  "Login",
+                  "Digite seu nome de usuário",
+                  textController: _ctrlLogin,
+                  validator: _validateLogin,
+                ),
+                _textFormField(
+                  "Senha",
+                  "Digite sua senha",
+                  isPassword: true,
+                  textController: _ctrlPassword,
+                  validator: _validatePassword,
+                ),
+                SizedBox(height: 30),
+                _raisedButton("Entrar", Colors.blue, context),
+              ],
+            ),
+          ),
+        );
+      }
+    );
+  }
+
+  _textFormField(
+    String label, 
+    String hint, {
+      bool isPassword = false, 
+      TextEditingController textController, 
+      FormFieldValidator<String> validator 
+    }) {
+    
+    return TextFormField(
+      controller: textController,
+      validator: validator,
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
       ),
     );
   }
+
+  String _validateLogin(String text) {
+    if(text.isEmpty){
+      return "Informe o login!";
+    }
+
+    if(text.length < 3) {
+      return "O usuário precisa ser maior do que 3 caracteres...";
+    }
+
+    return null;
+  }
+
+  String _validatePassword(String text) {
+    if(text.isEmpty){
+      return "Informe o senha!";
+    }
+
+    if(text.length < 3) {
+      return "A senha precisa ser maior do que 3 caracteres...";
+    }
+
+    return null;
+  }
+
+  _raisedButton(String text, Color color, BuildContext context) {
+    return RaisedButton(
+      padding: EdgeInsets.all(10),
+      color: color,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20
+        ),
+      ),
+      onPressed: () {
+        _clickButton(context);
+      },
+    );
+  }
+
+  _clickButton(BuildContext context) {
+    bool isFormValidated = _formKey.currentState.validate();
+
+    if(!isFormValidated) {
+      return ;
+    }
+
+    String login = _ctrlLogin.text;
+    String password = _ctrlPassword.text;
+
+    print("login: $login ||| senha: $password");
+  }
+
 }
